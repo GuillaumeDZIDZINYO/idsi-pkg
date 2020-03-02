57d1def2abc9f4c7660bbd9f7a99912a38b47401
Komla DZIDZINYO <kodzidzinyo2089@gmail.com>
2020-03-02 23:37:45 +0000

    ajout de setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='idsi_pkg',
+    version='1.0.0',
+    author='master2@idsi.ci',
+    description='Notre package',
+    packages=setuptools.find_packages(),
+    install_requires=[]
+	)
